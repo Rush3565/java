7a376748cdfb74f564af025cc3eff972f7b7c8e0
rushan <kazirush4@gmail.com>
2024-12-04 16:55:51 +0530

    "Added generic method printArray and main method to test it with String and Integer arrays."

---
+++ generic method/genericClass.java
@@ -0,0 +1,20 @@
+class generic <T>{
+    T i;
+    public void add(T i1){
+        i = i1;
+    }
+    public T get(){
+        return (i) ;
+    }
+}
+public class genericClass {
+    public static void main(String[] args) {
+        generic <Integer> g1 = new generic<Integer>();
+        generic <String> g2 = new generic<String>();
+        // Integer k = new Integer(5);
+        Integer k = 5;
+        g1.add(k);
+        g2.add("rahul");
+        System.out.println("g1 :" + g1.get() + " g2 :" + g2.get());
+    }
+}
--- generic method/genericMethod.java
+++ generic method/genericMethod.java
@@ -1,5 +1,17 @@
-package generic method;
-
 public class genericMethod {
-    
+    public <E> void printArray(E []inputArray) {
+        // for (int i = 0; i < inputArray.length; i++) {
+        //     System.out.println(inputArray[i]);
+        // }
+        for (E element : inputArray) {
+                    System.out.println(element);
+                }
+    }
+    public static void main(String[] args) {
+        genericMethod e1 = new genericMethod();
+        String countries[] = new String[]{"india ","pakistan","bangladesh"};
+        Integer numbers[]={10,20,30,40,50};
+        e1.printArray(countries); 
+        e1.printArray(numbers); 
+    }
 }
